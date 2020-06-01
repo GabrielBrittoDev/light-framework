fba2ed17de077eaf9d3cf64c36d3a916a744af20
Gabriel Leal <gabrielleao.mb@gmail.com>
2020-06-01 15:43:10 -0300

    Rebase index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-phpinfo();
+use core\Core;
+require_once '../vendor/autoload.php';
+
+// load our environment files - used to store credentials & configuration
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+require_once '../lib/autoload/autoload.php';
+require_once '../lib/database/Connection.php';
+require_once '../app/core/Core.php';
+require_once '../app/core/Router.php';
+require_once '../app/core/BaseController.php';
+
+$core = new Core;
+$core->start();
